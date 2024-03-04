8157bb2cb30d4ef2422e497c09a747f913d66a7d
MarcosBaez42 <146898216+MarcosBaez42@users.noreply.github.com>
2024-03-04 15:21:15 -0500

    proyecto terminado

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
 // Define la información de cada cuadro (imagen, texto, precio y botón)
 const cuadros = [
-    { texto: 'pantalon jean boyfriend', img: './img/pant1.png', precio: '63000.00' },
-    { img: './img/pant2.png', texto: 'Pantalon Cargo jean', precio: '70000.00' },
-    { img: './img/pant3.png', texto: 'Pantalon Cargo Camuflado', precio: '43000.00' },
-    { img: './img/pant4.png', texto: 'pantalon tiro alto con pretina', precio: '57000.00' },
-    { img: './img/pant5.png', texto: 'Pantalon de jean skinny con rotos', precio: '65000.00' },
-    { img: './img/pant6.png', texto: 'Pantalon bota recta cuerina', precio: '85000.00' },
-    { img: './img/pant7.png', texto: 'Pantalon bota recta de flores', precio: '62000.00' },
-    { img: './img/pant8.png', texto: 'Pantalon cargo correa', precio: '80000.00' },
-];
-
-// Obtén el contenedor
-const contenedor = document.getElementById('contenedor');
-
-// Genera dinámicamente los cuadros con imágenes, textos, precios y botones
-cuadros.forEach((cuadro, index) => {
+    { texto: 'pantalon jean boyfriend', img: './img/pant1.png', precio: '63000' },
+    { img: './img/pant2.png', texto: 'Pantalon Cargo jean', precio: '70000' },
+    { img: './img/pant3.png', texto: 'Pantalon Cargo Camuflado', precio: '43000' },
+    { img: './img/pant4.png', texto: 'pantalon tiro alto con pretina', precio: '57000' },
+    { img: './img/pant5.png', texto: 'Pantalon de jean skinny con rotos', precio: '65000' },
+    { img: './img/pant6.png', texto: 'Pantalon bota recta cuerina', precio: '85000' },
+    { img: './img/pant7.png', texto: 'Pantalon bota recta de flores', precio: '62000' },
+    { img: './img/pant8.png', texto: 'Pantalon cargo correa', precio: '80000' },
+  ];
+  
+  // Obtén el contenedor
+  const contenedor = document.getElementById('contenedor');
+  
+  // Genera dinámicamente los cuadros con imágenes, textos, precios y botones
+  cuadros.forEach((cuadro, index) => {
     const divCuadro = document.createElement('div');
     divCuadro.className = 'cuadro';
-
+  
     const img = document.createElement('img');
     img.className = `imgpant imgpant${index + 1}`;
     img.src = cuadro.img;
-
+  
     // Cambia la imagen al pasar el cursor
     img.addEventListener('mouseover', () => {
-        img.src = `./img/pant${index + 1}(2).png`;
+      img.src = `./img/pant${index + 1}(2).png`;
     });
-
+  
     // Restaura la imagen original al salir del cuadro
     img.addEventListener('mouseout', () => {
-        img.src = cuadro.img;
+      img.src = cuadro.img;
     });
-
+  
     const h3 = document.createElement('h3');
     h3.textContent = cuadro.texto;
-
-    const precio = document.createElement('p');
-    precio.textContent = `Precio: ${cuadro.precio}`;
-
+  
+    const precio = Math.trunc(parseFloat(cuadro.precio)); // Convierte a número entero
+  
+    const precioTexto = document.createElement('p');
+    precioTexto.textContent = `Precio: $${formatearNumero(precio)}`;
+  
     const botonComprar = document.createElement('button');
     botonComprar.textContent = 'Agregar Al Carro';
     botonComprar.className = 'buttonComprar'; // Asigna la clase al botón
-
+  
     divCuadro.appendChild(img);
     divCuadro.appendChild(h3);
-    divCuadro.appendChild(precio);
+    divCuadro.appendChild(precioTexto);
     divCuadro.appendChild(botonComprar);
 
     contenedor.appendChild(divCuadro);
@@ -94,6 +96,16 @@ function eliminarDelCarrito(id) {
     actualizarCarrito();
 }
 
+function formatearNumero(numero) {
+    if (typeof numero === 'number') {
+        // Convertir números a cadena y quitar decimales
+        numero = numero.toFixed(0);
+    }
+
+    // Separador de miles
+    return numero.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
 function actualizarCarrito() {
     carritoLista.innerHTML = '';
     let totalPrecio = 0;
@@ -104,8 +116,7 @@ function actualizarCarrito() {
             <td>${item.id + 1}</td>
             <td>${item.texto}</td>
             <td>${item.cantidad}</td>
-            <td>$${item.precio.toFixed(2)}</td>
-            <td>$${(item.precio * item.cantidad).toFixed(2)}</td>
+            <td>$${formatearNumero(item.precio.toFixed(2))}</td>
             <td><span class="eliminar" onclick="eliminarDelCarrito(${item.id})">❌</span></td>
         `;
         carritoLista.appendChild(listItem);
@@ -115,7 +126,7 @@ function actualizarCarrito() {
     const cantidadTotal = carrito.reduce((total, item) => total + item.cantidad, 0);
     cantidadCarrito.textContent = cantidadTotal;
 
-    total.textContent = `Total: $${totalPrecio.toFixed(2)}`;
+    total.textContent = `Total: $${formatearNumero(totalPrecio.toFixed(2))}`;
 
     // Mostrar la cesta siempre
     cesta.style.display = 'block';
@@ -132,11 +143,13 @@ document.querySelector('.carrito').addEventListener('click', () => {
     cesta.style.display = (cesta.style.display === 'none' || cesta.style.display === '') ? 'block' : 'none';
 });
 
+function vaciarCarrito() {
+    carrito.length = 0;
+    actualizarCarrito();
+}
+
 function comprarArticulos() {
-    // Aquí puedes agregar lógica adicional para procesar la compra
-    // Por ejemplo, puedes mostrar un mensaje de éxito o redirigir a una página de pago.
     alert('¡Compra exitosa!');
-    // Luego, puedes reiniciar el carrito si es necesario
     carrito.length = 0;
     actualizarCarrito();
 }
